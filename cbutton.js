1ddc3649d91fa5b81af84483cccb7939350e4e0e
DESKTOP-ESNRABV\Ariful Islam <ariful@omnisolutionsbd.com>
2017-12-21 23:40:11 +0600

    Custome button

--- cbutton.js
+++ cbutton.js
@@ -1,31 +1,35 @@
-let html = ` 
-    <style>
-        .cbtn {
-            transform: rotateZ(10deg);
-        }
-    </style>
-    <button class="cbtn"></button>
-`
-
 class CButton extends HTMLElement {
+    get button() {
+        return this._button;
+    }
+    set button(v) {
+        this._button = v;
+    }
     constructor() {
         super();
-        this.Init()
+    }
+    connectedCallback() {
+        this.Init();
     }
     Init() {
-        this.Shadow = this.attachShadow({ mode: 'open' })
-        this.Shadow.innerHTML = html
-        let btn = this.Shadow.querySelector(".cbtn")
-        let txt = this.getAttribute("text")
-        btn.innerHTML = txt
-        let n = this.getAttribute("repeat")
-        if (n) {
-            let nn = parseInt(n);
-            for (let index = 1; index < nn; index++) {
-                btn.innerHTML += " " + txt
-            }
-        }
+        let elm = document.createElement('button');
+        elm.innerHTML = this.getAttribute("text");
+        elm.style.backgroundColor = 'rgb(255,255,255)';
+        elm.style.lineHeight = '1.5em';
+        elm.style.display = 'inline-block';
+        elm.style.boxShadow = '2px 2px 2px 2px rgba(175, 170, 170, 0.73)';
+        elm.style.border = 'none';
+        elm.style.borderRadius = '2px';
+        elm.addEventListener('mouseenter', this.onMouseEnter);
+        elm.addEventListener('mouseleave', this.onMouseLeave);
+        this.button = elm;
+        this.appendChild(elm);
     }
-
+    onMouseEnter(evt) {
+        evt.target.style.boxShadow = '1px 1px 1px 1px rgba(175, 170, 170, 0.73)';
+    }
+    onMouseLeave(evt) {
+        evt.target.style.boxShadow = '2px 2px 2px 2px rgba(175, 170, 170, 0.73)';
+    }
 }
 window.customElements.define('c-button', CButton);
